5ed9166ae63bd889aa1392e6a3c057821023b7f5
ris <alwaris.dr@gmail.com>
2017-10-21 11:12:18 +0700

    Initial commit

---
+++ app/src/main/java/com/ilmis/hellokotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ilmis.hellokotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        helloMe.setOnClickListener{(toast("Hello People"))}
+    }
+}
+
